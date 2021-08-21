7c0c8738662f255ebbc8ba63f385133a5f36bab9
Rono <ronodennis580@gmail.com>
2021-08-21 11:36:36 +0300

    Ternary operator example

--- src/LargestSmallest.java
+++ src/LargestSmallest.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class LargestSmallest {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int a, b;
+        String result;
+        System.out.println("Enter num 1");
+        a=in.nextInt();
+        System.out.println("Enter num 2");
+        b=in.nextInt();
+        //ternary operator
+        result=(a>b)?"num 1["+a+" ]is greater than num2 [" +b+"]":"num 1["+a+" ]is less than num2 [" +b+"]";
+        System.out.println(result);
+    }
 }
